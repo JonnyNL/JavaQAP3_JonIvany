b5c20cdb826b2f0a1148d4425c894001052d64e3
Jon Ivany <117655972+JonnyNL@users.noreply.github.com>
2023-07-04 16:11:17 -0230

    Missed changes

--- Problem1/Student.java
+++ Problem1/Student.java
@@ -1,29 +1,36 @@
 class Student extends Person {
+    // Protected variables
     protected String myIdNum;
     protected double myGPA;
 
+    // Constructor for student
     public Student(String name, int age, String gender, String idNum, double gpa) {
         super(name, age, gender);
         myIdNum = idNum;
         myGPA = gpa;
     }
 
+    // Getter for student id
     public String getIdNum() {
         return myIdNum;
     }
 
+    // Getter for student gpa
     public double getGPA() {
         return myGPA;
     }
 
+    // Setter for student id
     public void setIdNum(String idNum) {
         myIdNum = idNum;
     }
 
+    // Setter for student gpa num
     public void setGPA(double gpa) {
         myGPA = gpa;
     }
 
+    // Student to string
     public String toString() {
         return super.toString() + ", ID: " + myIdNum + ", GPA: " + myGPA;
     }
